e12b916048b7e1a698dfee2cf55575a62f37776d
kristaps <kristapsbulssusons@gmail.com>
2022-02-11 01:23:35 +0200

    [fix]

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const moment = require('moment');
 
 const generateMessage = (username, message) => ({
@@ -16,23 +15,4 @@ const generateLocationMessage = (username, message) => ({
 module.exports = {
     generateMessage,
     generateLocationMessage
-=======
-const moment = require('moment');
-
-const generateMessage = (username, message) => ({
-    username,
-    text: message,
-    created_at: moment().format('MM-DD-YY HH:MM:ss'),
-});
-
-const generateLocationMessage = (username, message) => ({
-    username,
-    url: message,
-    created_at: moment().format('MM-DD-YY HH:MM:ss'),
-});
-
-module.exports = {
-    generateMessage,
-    generateLocationMessage
->>>>>>> 3ae7402df9be7c61751413d76c4726eed427237c
 }
